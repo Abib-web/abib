003f131643153e3b02532b222c40272b29d5b253
oumar <konehabiboullah@gmail.com>
2025-03-02 10:56:49 -0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
-import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import AllProjects from "./components/AllProjects";
-import Header from "./components/Header/Header"; // Importation du Header
-import './App.css'
 
-function App() {
-  return (
-    <Router>
-      <Header /> {/* Le Header s'affiche sur toutes les pages */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<AllProjects />} />
-      </Routes>
-    </Router>
-  );
-}
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
@@ -39,6 +20,5 @@ const App = () => {
     </Router>
   );
 };
->>>>>>> gh-pages
 
 export default App;
